e5c58f92fc7c1b2adb768214061f2922a7568f78
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-02-16 10:32:41 +0200

    hw1

--- App.js
+++ App.js
@@ -1,43 +1,49 @@
 const fs = require('fs');
 const path = require('path')
 
+// Створити основну папку (main), в яку покласти дві інші папки: перша - online, друга - inPerson
 
-// fs.mkdir(path.join(__dirname, 'main'),(err)=>{
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log('Directody is creasted')
-// })
+fs.mkdir(path.join(__dirname, 'main'),(err)=>{
+    if (err) {
+        console.log(err)
+    }
+    console.log('Directody is creasted')
+})
+
+fs.mkdir(path.join(__dirname,'main','online'),(err)=>{
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
+fs.mkdir(path.join(__dirname,'main','inPerson'),(err)=>{
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
 //
-// fs.mkdir(path.join(__dirname,'main','online'),(err)=>{
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-// fs.mkdir(path.join(__dirname,'main','inPerson'),(err)=>{
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
+// Потім створити в вашому головному файлі (для прикладу app.js) два масиви з обєктами user ({. name: "Andrii", age: 22, city: "Lviv" }),
+// відповідно перший - onlineUsers, другий - inPersonUsers;
+// і створити файли txt в папках (online, inPerson) в яких як дату покласти юзерів з ваших масивів,
+// але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
 
 
-// fs.writeFile(path.join(__dirname,'main','inPerson','inPerson.txt'),'', (err)=>{
-//     if (err) {
-//         console.log(err);
-//         throw err;
-//     }
-// })
+fs.writeFile(path.join(__dirname,'main','inPerson','inPerson.txt'),'', (err)=>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
 
 
-//
-// fs.writeFile(path.join(__dirname,'main','online','online.txt'),'' ,(err)=>{
-//     if (err) {
-//         console.log(err);
-//         throw err;
-//     }
-// })
+
+fs.writeFile(path.join(__dirname,'main','online','online.txt'),'' ,(err)=>{
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+})
 
 
 let onlineUsers =
@@ -56,41 +62,43 @@ let inPersonUsers =
     ]
 
 
-// fs.unlink(path.join(__dirname,'main','online','online.txt'),(err)=>{
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
+fs.unlink(path.join(__dirname,'main','online','online.txt'),(err)=>{
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
 
 
-// fs.appendFile(path.join(__dirname,'main','inPerson','inPerson.txt'),JSON.stringify([inPersonUsers],null,1), (err)=> {
-//     if (err) {
-//         console.log(err);
-//     }
-// })
+fs.appendFile(path.join(__dirname,'main','inPerson','inPerson.txt'),JSON.stringify([inPersonUsers],null,1), (err)=> {
+    if (err) {
+        console.log(err);
+    }
+})
 
+
+fs.appendFile(path.join(__dirname,'main','online','online.txt'),JSON.stringify([onlineUsers],null,1), (err)=> {
+    if (err) {
+        console.log(err);
+    }
+})
+
+// Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу.
+// (ті, що були в папці inPerson будуть в папці online)
+
+
+// fs.rename(path.join(__dirname, 'main', 'inPerson', 'inPerson.txt'),
+//     path.join(__dirname, 'main', 'online', 'inPerson.txt'), (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         })
 //
-// fs.appendFile(path.join(__dirname,'main','online','online.txt'),JSON.stringify([onlineUsers],null,1), (err)=> {
-//     if (err) {
-//         console.log(err);
-//     }
-// })
-
-
-
-fs.rename(path.join(__dirname, 'main', 'inPerson', 'inPerson.txt'),
-    path.join(__dirname, 'main', 'online', 'inPerson.txt'), (err) => {
-        if (err) {
-            console.log(err)
-        }
-        })
-
-fs.rename(path.join(__dirname, 'main', 'online', 'online.txt'),
-    path.join(__dirname, 'main', 'inPerson', 'online.txt'), (err) => {
-        if (err) {
-            console.log(err)
-        }
-        })
+// fs.rename(path.join(__dirname, 'main', 'online', 'online.txt'),
+//     path.join(__dirname, 'main', 'inPerson', 'online.txt'), (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         })
 
 
