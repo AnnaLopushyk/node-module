bce1a2aa78171df393857254ce688140302dcff5
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-02-16 10:58:16 +0200

    hw1

--- App.js
+++ App.js
@@ -2,48 +2,31 @@ const fs = require('fs');
 const path = require('path')
 
 // Створити основну папку (main), в яку покласти дві інші папки: перша - online, друга - inPerson
-
-fs.mkdir(path.join(__dirname, 'main'),(err)=>{
-    if (err) {
-        console.log(err)
-    }
-    console.log('Directody is creasted')
-})
-
-fs.mkdir(path.join(__dirname,'main','online'),(err)=>{
-    if(err){
-        console.log(err);
-        throw err;
-    }
-})
-fs.mkdir(path.join(__dirname,'main','inPerson'),(err)=>{
-    if(err){
-        console.log(err);
-        throw err;
-    }
-})
+//
+// fs.mkdir(path.join(__dirname, 'main'),(err)=>{
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log('Directody is creasted')
+// })
+//
+// fs.mkdir(path.join(__dirname,'main','online'),(err)=>{
+//     if(err){
+//         console.log(err);
+//         throw err;
+//     }
+// })
+// fs.mkdir(path.join(__dirname,'main','inPerson'),(err)=>{
+//     if(err){
+//         console.log(err);
+//         throw err;
+//     }
+// })
 //
 // Потім створити в вашому головному файлі (для прикладу app.js) два масиви з обєктами user ({. name: "Andrii", age: 22, city: "Lviv" }),
 // відповідно перший - onlineUsers, другий - inPersonUsers;
 // і створити файли txt в папках (online, inPerson) в яких як дату покласти юзерів з ваших масивів,
 // але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
-
-
-fs.writeFile(path.join(__dirname,'main','inPerson','inPerson.txt'),'', (err)=>{
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-})
-
-
-
-fs.writeFile(path.join(__dirname,'main','online','online.txt'),'' ,(err)=>{
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-})
 
 
 let onlineUsers =
@@ -61,27 +44,24 @@ let inPersonUsers =
         {NAME: 'Denys', AGE: 25, CITY: 'Lviv'}
     ]
 
+//
+// onlineUsers.forEach(user => {
+//     fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'inPerson.txt'), JSON.stringify([inPersonUsers], null, 1), (err) => {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     })
+// })
+// inPersonUsers.forEach(user => {
+//     fs.writeFile(path.join(__dirname, 'main', 'online', 'online.txt'), JSON.stringify([onlineUsers], null, 1), (err) => {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     })
+// })
 
-fs.unlink(path.join(__dirname,'main','online','online.txt'),(err)=>{
-    if(err){
-        console.log(err);
-        throw err;
-    }
-})
-
-
-fs.appendFile(path.join(__dirname,'main','inPerson','inPerson.txt'),JSON.stringify([inPersonUsers],null,1), (err)=> {
-    if (err) {
-        console.log(err);
-    }
-})
-
-
-fs.appendFile(path.join(__dirname,'main','online','online.txt'),JSON.stringify([onlineUsers],null,1), (err)=> {
-    if (err) {
-        console.log(err);
-    }
-})
 
 // Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу.
 // (ті, що були в папці inPerson будуть в папці online)
@@ -100,5 +80,4 @@ fs.appendFile(path.join(__dirname,'main','online','online.txt'),JSON.stringify([
 //             console.log(err)
 //         }
 //         })
-
 
